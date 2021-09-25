d18831384f500ce17bd9c94201a5da5d0e04fffc
Thanasis Mattas <atmattas@physics.auth.gr>
2021-09-26 00:31:49 +0300

    add: 12 Graph Traversal

---
+++ src/12_Graph_Traversal/bfs_dfs.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& v)
+{
+  out << '[';
+  if (!v.empty()) {
+    copy(v.begin(), v.end(), ostream_iterator<int>(out, ", "));
+    out << "\b\b";
+  }
+  out << ']';
+  return out;
+}
+
+
+vector<int> bfs(const vector<unordered_set<int> >& adj, int s)
+{
+  bool disc[adj.size() + 1];
+  memset(disc, false, sizeof(disc));
+  disc[s] = true;
+  vector<int> seq;
+  queue<int> q;
+  q.push(s);
+
+  while (!q.empty()) {
+    int u = q.front(); q.pop();
+    seq.push_back(u);
+    for (auto v : adj[u]) {
+      if (!disc[v]) {
+        disc[v] = true;
+        q.push(v);
+      }
+    }
+  }
+  return seq;
+}
+
+
+vector<int> dfs(const vector<unordered_set<int> >& adj, int s)
+{
+  bool disc[adj.size() + 1];
+  memset(disc, false, sizeof(disc));
+  disc[s] = true;
+  stack<int> to_visit;
+  to_visit.push(s);
+  vector<int> seq;
+
+  while (!to_visit.empty()) {
+    int u = to_visit.top(); to_visit.pop();
+    seq.push_back(u);
+    for (auto v : adj[u]) {
+      if (!disc[v]) {
+        disc[v] = true;
+        to_visit.push(v);
+      }
+    }
+  }
+  return seq;
+}
+
+
+int main()
+{
+  vector<unordered_set<int> > adj{
+    unordered_set<int>{},
+    unordered_set<int>{2, 6, 10},
+    unordered_set<int>{1, 3, 5},
+    unordered_set<int>{2, 4},
+    unordered_set<int>{3, 5, 6},
+    unordered_set<int>{2, 4},
+    unordered_set<int>{1, 4, 7},
+    unordered_set<int>{6, 8, 12},
+    unordered_set<int>{7, 9, 12},
+    unordered_set<int>{10, 8, 11},
+    unordered_set<int>{1, 9},
+    unordered_set<int>{9},
+    unordered_set<int>{7, 8}
+  };
+
+  cout << bfs(adj, 1) << '\n';
+  cout << dfs(adj, 1) << '\n';
+}
