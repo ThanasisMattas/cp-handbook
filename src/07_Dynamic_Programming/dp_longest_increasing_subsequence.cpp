4438600a82c5ee1a3eb60ad59e64ae407694f3da
Thanasis Mattas <atmattas@physics.auth.gr>
2021-12-17 18:51:41 +0200

    add: dp_longest_increasing_subsequence nlogn

--- src/07_Dynamic_Programming/dp_longest_increasing_subsequence.cpp
+++ src/07_Dynamic_Programming/dp_longest_increasing_subsequence.cpp
@@ -16,30 +16,47 @@ ostream& operator<<(ostream& out, const vector<T>& v)
 }
 
 
-vector<int> arr{6, 2, 5, 1, 7, 4, 8, 3};
-vector<int> len(arr.size(), 1);
+const int n = 8;
+vector<int> v{6, 2, 5, 1, 7, 4, 8, 3};
+vector<int> len(n, 1);
 
 
+// O(n^2)
 void solve()
 {
   int best = 1;
 
-  for (size_t k = 0; k < arr.size(); ++k) {
+  for (size_t k = 1; k < v.size(); ++k) {
     for (size_t i = 0; i < k; ++i) {
-      if (arr[i] < arr[k]) {
+      if (v[i] < v[k]) {
         len[k] = max(len[k], len[i] + 1);
       }
     }
     best = max(best, len[k]);
   }
-  cout << arr << '\n';
+  cout << v << '\n';
   cout << len << "\n";
   cout << best << "\n";
 }
 
 
-int main()
+vector<int> seq(n);
+
+// O(nlogn)
+void solve2()
 {
-  solve();
+  // points to the 1st empty slot in seq
+  // (used to pre-alocate the memory of seq)
+  int seq_len = 1;
+  seq[0] = v[0];
+
+  for (int k = 1; k < n; ++k) {
+    auto it = lower_bound(seq.begin(), seq.begin() + seq_len, v[k]);
+    if (it == seq.begin() + seq_len) seq[seq_len++] = v[k];
+    else *it = v[k];
+  }
+  cout << seq_len << '\n';
 }
 
+
+int main() {solve2();}
