2a11e79bd67f3b8921ff42fb96c4015b5ebfc5d5
Thanasis Mattas <atmattas@physics.auth.gr>
2021-12-31 00:03:10 +0200

    refactor: no need to init stack at nearest_smaller_element

--- src/02_Time_complexity/max_subarray_sum.cpp
+++ src/02_Time_complexity/max_subarray_sum.cpp
@@ -1,5 +1,4 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
 
@@ -9,11 +8,11 @@ vector<int> v{-1, 2, 4, -3, 5, 2, -5, 2};
 // O(n)
 int main()
 {
-  int sum = 0, best = 0;
-  for (const int& i : v) {
+  int sum = 0;
+  int best = 0;
+  for (auto i : v) {
     sum = max(sum + i, i);
     best = max(best, sum);
   }
-
   cout << best << '\n';
 }
--- src/08_Amortized_Analysis/nearest_smaller_element.cpp
+++ src/08_Amortized_Analysis/nearest_smaller_element.cpp
@@ -19,22 +19,16 @@ vector<int> v{1, 3, 4, 2, 5, 3, 4, 2, 0, 1};
 void solve()
 {
   cout << v << '\n';
-  cout << "  ";
-
   stack<int> s;
-  s.push(v[0]);
 
-  for (size_t i = 1; i < v.size(); ++i) {
-    while ((!s.empty()) && (s.top() >= v[i])) s.pop();
+  for (auto c : v) {
+    while ((!s.empty()) && (s.top() >= c)) s.pop();
     if (!s.empty()) cout << s.top() << ' ';
     else cout << "- ";
-    s.push(v[i]);
+    s.push(c);
   }
   cout << '\n';
 }
 
 
-int main()
-{
-  solve();
-}
+int main() {solve();}
