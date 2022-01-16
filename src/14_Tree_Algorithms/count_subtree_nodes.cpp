55988878ad38f23390b4cd9bde2faab838a03ab9
Thanasis Mattas <atmattas@physics.auth.gr>
2022-01-16 14:38:20 +0200

    add: count_subtree_nodes

---
+++ src/14_Tree_Algorithms/count_subtree_nodes.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+//              1
+//        2           3
+//    4       5   6       7
+// 8     9            12
+//     10 11
+const int n = 12;
+vector<int> adj[n + 1] = {
+  {},
+  {2, 3},       //  1
+  {1, 4, 5},    //  2
+  {1, 6, 7},    //  3
+  {2, 8, 9},    //  4
+  {2},          //  5
+  {3},          //  6
+  {3, 12},      //  7
+  {4},          //  8
+  {4, 10, 11},  //  9
+  {9},          // 10
+  {9},          // 11
+  {7}           // 12
+};
+int counter[n + 1];
+
+
+void dfs(int u, int u_prev)
+{
+  counter[u] = 1;
+  for (auto u_next : adj[u]) {
+    if (u_next == u_prev) continue;
+    dfs(u_next, u);
+    counter[u] += counter[u_next];
+  }
+}
+
+
+int main()
+{
+  dfs(1, 0);
+
+  for (int i = 1; i <= n; ++i)
+    cout << setw(2) << i << " : " << counter[i] << '\n';
+}
+
