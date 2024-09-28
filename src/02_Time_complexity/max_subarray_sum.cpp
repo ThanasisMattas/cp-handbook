729f48e8ed7e9a024733f76574442ce157d4390a
Thanasis Mattas <atmattas@physics.auth.gr>
2024-09-28 17:48:27 +0300

    fix: best subarray sum is init at -inf

--- src/02_Time_complexity/max_subarray_sum.cpp
+++ src/02_Time_complexity/max_subarray_sum.cpp
@@ -6,10 +6,10 @@ vector<int> v{-1, 2, 4, -3, 5, 2, -5, 2};
 
 
 // O(n)
-int main()
+void solve()
 {
   int sum = 0;
-  int best = 0;
+  int best = -1e9;
   for (auto i : v) {
     sum = max(sum + i, i);
     best = max(best, sum);
