d3ee86ca50737198995aaf5b4dd348230c740676
Thanasis Mattas <atmattas@physics.auth.gr>
2025-02-23 17:48:02 +0200

    doc: greedy_coins complexity

--- src/06_Greedy_algorithms/greedy_coins.py
+++ src/06_Greedy_algorithms/greedy_coins.py
@@ -2,14 +2,17 @@ euro_coins = [1, 2, 5, 10, 20, 50, 100, 200]
 rand_coins = [1, 3 ,4]
 
 
+# O(n * k), k = len(coins)
+# Explanation
+# -----------
+# outer loop: k times
+# inner loop: each coin contributes n / coin iterations, which is bounded to n
 def solve(n, coins):
   counter = 0
-
   for coin in reversed(coins):
     while n - coin >= 0:
       n -= coin
       counter += 1
-
   return counter
 
 if __name__ == '__main__':
