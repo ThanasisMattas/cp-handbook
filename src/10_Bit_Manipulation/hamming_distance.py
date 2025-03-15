67ea396616c66167e0e34961f87df12a8ca655dc
Thanasis Mattas <atmattas@physics.auth.gr>
2025-03-15 15:58:35 +0200

    perf: compare str-based vs int-based humming dist

--- src/10_Bit_Manipulation/hamming_distance.py
+++ src/10_Bit_Manipulation/hamming_distance.py
@@ -1,6 +1,7 @@
 from itertools import combinations
+from timeit import timeit
 
-bitstrings = [
+bitstrs = [
   "01110001",
   "10100101",
   "11101001",
@@ -13,14 +14,32 @@ bitstrings = [
   "11111000"
 ]
 
+# O(k), where k is the length of the bitstr
+def hamming_dist_str(a: str, b:str) -> int:
+  """Can be used with any pair of strings (not just bitstrigns)."""
+  if len(a) != len(b):
+    raise ValueError("Strings must be of the same length")
+  return sum(c1 != c2 for c1, c2 in zip(a, b))
+
+
+# O(n^2 * k), where k is the length of the bitstr
+def solve_str():
+  return min(hamming_dist_str(a, b) for a, b in combinations(bitstrs, 2))
+
 
 # O(n^2) - combinations: C(n,2) = n(n-1)/2
-def solve():
-  ints = [int(bitstring, 2) for bitstring in bitstrings]
+def solve_int():
+  ints = [int(bitstr, 2) for bitstr in bitstrs]
   return min((a ^ b).bit_count() for a, b in combinations(ints, 2))
 
 
 if __name__ == '__main__':
-  best = solve()
-  print(best)
-  assert best == 2
+  best_int = solve_int()
+  best_str = solve_str()
+  print(best_int)
+  assert best_int == best_str == 2
+
+  print("str-based time:", end=' ')
+  print(round(timeit(solve_str, number=10000), 3))
+  print("int-based time:", end=' ')
+  print(round(timeit(solve_int, number=10000), 3))
