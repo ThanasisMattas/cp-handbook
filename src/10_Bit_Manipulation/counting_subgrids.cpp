bc3467f7d49ba6806d9ee2d8a994dce108abf625
Thanasis Mattas <atmattas@physics.auth.gr>
2025-03-15 23:56:30 +0200

    fix: counting_subgrids: compress grid + use bitset

--- src/10_Bit_Manipulation/counting_subgrids.cpp
+++ src/10_Bit_Manipulation/counting_subgrids.cpp
@@ -1,47 +1,67 @@
 #include <bits/stdc++.h>
+#include "../compete.hpp"
 using namespace std;
 
 
-const int n = 512;
+const int n = 1500;
 const int N = 64;
+const int num_blocks = (n + N - 1) / N;
 int color[n][n];
-long long color_bset[n][n / N];
+bitset<N> color_bset[n][num_blocks];
 
-// random_device rd;
+random_device rd;
 // mersenne_twister_engine
-mt19937_64 rng(time(nullptr));
-// std::uniform_int_distribution<> distrib(1, 6);
-uniform_int_distribution<long long> dist;
+mt19937_64 rng(rd());
+uniform_int_distribution<int> dist(0, 1);
 
 
 // O(n^3)
-int brute_force()
+long long solve_brute_force()
 {
-  int total = 0;
+  Timer t(__func__);
+
+  long long total = 0;
   for (int i = 0; i < n - 1; ++i) {
     for (int j = i + 1; j < n; ++j) {
-      int count = 0;
+      long long count = 0;
       for (int k = 0; k < n; ++k) {
         if (color[i][k] && color[j][k]) ++count;
       }
-      total += count * (count - 1) / 2;
+      total += (count * (count - 1)) / 2;
     }
   }
   return total;
 }
 
 
-// O(n^3/N)
-int bit_optimization()
+void compress_color_matrix()
 {
-  int total = 0;
+  Timer t(__func__);
+
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      int block = j / N;
+      int bit = j % N;
+      color_bset[i][block][bit] = color[i][j];
+    }
+  }
+}
+
+
+// O(n^3/N)
+long long bit_optimization()
+{
+  Timer t(__func__);
+
+  long long total = 0;
   for (int i = 0; i < n - 1; ++i) {
     for (int j = i + 1; j < n; ++j) {
-      int count = 0;
-      for (int k = 0; k < n / N; ++k) {
-        count += __builtin_popcount(color_bset[i][k] & color_bset[j][k]);
+      long long count = 0;
+      for (int k = 0; k < num_blocks; ++k) {
+        // count += __builtin_popcount(color_bset[i][k] & color_bset[j][k]);
+        count += (color_bset[i][k] & color_bset[j][k]).count();
       }
-      total += count * (count - 1) / 2;
+      total += (count * (count - 1)) / 2;
     }
   }
   return total;
@@ -50,13 +70,12 @@ int bit_optimization()
 
 int main()
 {
-  generate(color[0], color[0] + n * n, [](){return rand() % 2;});
-  generate(color_bset[0], color_bset[0] + n * n / N, [](){return dist(rng);});
+  generate(color[0], color[0] + n * n, [](){return dist(rng);});
 
-  for (int i = 0; i < n; ++i)
-    for (int j = 0; j < n; ++j)
-      cout << color[i][j] << ' ';
+  long long count_solve_brute_force = solve_brute_force();
+  compress_color_matrix();
+  long long count_bit_optim = bit_optimization();
 
-  cout << brute_force() << '\n';
-  cout << bit_optimization() << '\n';
+  assert(count_bit_optim == count_solve_brute_force);
+  cout << "n=" << n << "  N=" << N << "  count=" << count_bit_optim << '\n';
 }
