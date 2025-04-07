b08e0b59da5b193a9e0262e16ce277a96c4d397d
Thanasis Mattas <atmattas@physics.auth.gr>
2025-04-07 20:43:34 +0300

    refactor: kosaraju_pass() + doc

--- src/17_Strong_connectivity/kosaraju.py
+++ src/17_Strong_connectivity/kosaraju.py
@@ -8,6 +8,7 @@ adj = [
   [3, 5],
   [6]
 ]
+n = len(adj) - 1
 
 
 # O(V + E)
@@ -36,23 +37,26 @@ def dfs(start, visited):
 
 
 # O(V + E)
-def kosaraju():
-  n = len(adj) - 1
-
-  # 1st pass
-  #   Since nodes are naturally ordered, each DFS run groups reachable nodes
-  #   from each starting node. After flattening, we get the total post-order.
+def kosaraju_pass(nodes=range(1, n + 1)):
+  """Each DFS-run groups reachable nodes from each starting node (u)"""
   visited = [False] * (n + 1)
-  postorder = [dfs(u, visited) for u in range(1, n + 1) if not visited[u]]
-  postorder = [u for reachable in postorder for u in reachable]
+  return [dfs(u, visited) for u in nodes if not visited[u]]
+
+
+# O(V + E)
+def kosaraju():
+  # 1st pass
+  #   Get reachable groups from each unvisited node in post-order, and flatten.
+  postorder = [u for reachable in kosaraju_pass() for u in reachable]
 
   # 2nd pass
-  #   The post-order from the first pass already placed SCCs contiguously.
-  #   Thus, the reachable groups now will essentially be the SCCs. This
-  #   implementation avoids the reversal of the adjacency list from the
-  #   original algorithm.
-  visited = [False] * (n + 1)
-  return [dfs(u, visited) for u in postorder if not visited[u]]
+  #   The post-order from the first pass have sorted the nodes from leaf to
+  #   root, resulting in placing the SCCs contiguously. Using this sequence at
+  #   the second pass, the reachable groups now will be the SCCs.
+  #
+  #   This implementation avoids the reversal of the adjacency list as with
+  #   the original algorithm.
+  return kosaraju_pass(postorder)
 
 
 if __name__ == '__main__':
